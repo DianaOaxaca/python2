daba286e3a1e1f3813f7742c3ed849c1602d65f9
Diana Oaxaca <hoaxacadiana@gmail.com>
2021-09-13 00:36:04 -0500

    update exercise one

--- clases/ejercicios/seq_format_ex.py
+++ clases/ejercicios/seq_format_ex.py
@@ -1,23 +1,37 @@
-##ejercicio1
+##ejercicio1 Obtener cadena proteica de cualquiera de sus ORFs
+from os import makedev
 from Bio.Seq import Seq
+from Bio import SeqIO
 from Bio import SeqUtils
 from Bio.SeqUtils import nt_search, GC, molecular_weight
 secuencia2 = Seq("AGCCATGTAGCTAACTCAGGTTACATGGGGATGACCCCGCGACTTGGATTAGAGTCTCTTTTGGAATAAGCCTGAATGATCCGAGTAGCATCTCAG")
 orf = Seq("ATG")
 orf_posicion = nt_search(str(secuencia2), orf)
+#iniciar listas vacias
+proteinas = []
+longitudes = []
 for start in range(1,len(orf_posicion)): #1, que no me tome la primer posicion(en donde se encuentra el patron)
     #Se guarda secuencia en posicion de codon de inicio
     sec_prot = secuencia2[start:]
     #Traducir secuencia hasta codon de paro
     proteina = sec_prot.translate(to_stop=True)
-    print(proteina)
+    proteina = str(proteina)
+    #concatenar elementos
+    proteinas.append(proteina)
+prots = proteinas
+for i in prots:
+    sizes = len(i)
+    longitudes.append(sizes)
+#Obtener la proteina de mayor longitud
+largest = prots[longitudes.index(max(longitudes))]
+print("Largest protein is: {}".format(largest))
 
 ##Ejercicio 2
 #Imprimir codones (separados por un espacio)de cada secuencia en formato fasta
 #Usar diccionario, encontrar codones del primer marco de lectura, imprimir en fasta
 from Bio import SeqIO
 import re
-filename = "../../data/seq.nt.fa"
+filename = "/Users/diana/Documents/python2/data/seq.nt.fa"
 id_dict = SeqIO.to_dict(SeqIO.parse(filename, "fasta"))
 for i in id_dict:
     print(">{}".format(i))
@@ -28,7 +42,7 @@ for i in id_dict:
     print("\n")
 
 #Ejercicio 3
-path = "../../data/sample.fastq"
+path = "/Users/diana/Documents/python2/data/sample.fastq"
 def corte_calidad(path, umbral):
     mala_calidad = []
     for record in SeqIO.parse(path, "fastq"):
